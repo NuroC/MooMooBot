0b73342025332c62f6bc6b6eb4c18eaadf3bfbc2
Nuro <82734446+NuroC@users.noreply.github.com>
2021-12-25 15:59:54 -0700

    Update messageCreate.js
    add permissions to fill command and improve proxy fetching.

--- discord/messageCreate.js
+++ discord/messageCreate.js
@@ -10,6 +10,7 @@ const removeBots = require("../src/functions/removeBots.js");
 const getTaken = require("../src/strings/taken.js");
 const collectStats = require("../src/bot/serverStats.js");
 let Projects = require("../src/bot/projects.js");
+// const getStatsEmb = require("../src/strings/embeds/stats.js")
 Projects = Projects();
 var page, init;
 emitter.once("open", (arg1, arg2) => {
@@ -143,40 +144,68 @@ module.exports = async (client, message, serverData) => {
       removeBots(Projects);
       break;
     case "fill":
-      if (!args[0] || !args[0].includes(":"))
-        return message.channel.send("Invalid server.");
-      if (
-        !args[1] ||
-        !["normal", "norm", "sandbox", "sand", "dev"].includes(args[1])
-      )
-        return message.channel.send("Invalid server type.");
-      args[1] = args[1].includes("norm")
-        ? "normal"
-        : args[1].includes("sand")
-        ? "sandbox"
-        : "dev";
-      let ipv = await fetchIP(
-        args[0].split(":")[0],
-        args[0].split(":")[1].split(":")[0],
-        args[1]
-      );
-      if (!ipv) return message.channel.send("Invalid server.");
-      message.channel.send({
-        embeds: [
-          {
-            title: `Connecting Bots`,
-            description: `50 Bots will join ${args[1]} ${
-              args[0]
-            } as soon as possible.`
-          }
-        ]
-      });
-      for (let project in Projects) {
-        fetch(
-          `https://${Projects[project]}.glitch.me/sendbot?name=${
-            ["Nuro", "Wealthy"][(Math.random() * 2) | 0]
-          }&server=${args[0]}&type=${args[1]}&amount=4`
+      if (message.member.roles.cache.has("924397464079982654")) {
+        if (!args[0] || !args[0].includes(":"))
+          return message.channel.send("Invalid server.");
+        if (
+          !args[1] ||
+          !["normal", "norm", "sandbox", "sand", "dev"].includes(args[1])
+        )
+          return message.channel.send("Invalid server type.");
+        args[1] = args[1].includes("norm")
+          ? "normal"
+          : args[1].includes("sand")
+          ? "sandbox"
+          : "dev";
+        let ipv = await fetchIP(
+          args[0].split(":")[0],
+          args[0].split(":")[1].split(":")[0],
+          args[1]
         );
+        if (!ipv) return message.channel.send("Invalid server.");
+        let msgid;
+        message.channel.send({
+          embeds: [
+            {
+              title: `Connecting Bots`,
+              description: `50 Bots will join ${args[1]} ${
+                args[0]
+              } as soon as possible.`
+            }
+          ]
+        }).then(msg => {
+          msgid = msg.id;
+          console.log(msgid)
+        })
+        let sentProjects = 0;
+        let failedProxies = 0
+        for (let project in Projects) {
+           let response = await fetch(
+            `https://${Projects[project]}.glitch.me/sendbot?name=${
+              ["Nuro", "Wealthy"][(Math.random() * 2) | 0]
+            }&server=${args[0]}&type=${args[1]}&amount=4`
+          )
+           let text = await response.text()
+            if(text.split(" ")[0] == "Connecting") {
+              sentProjects++
+            } else {
+              failedProxies++
+            }
+        }
+        message.channel.messages.fetch({around: msgid, limit: 1})
+    .then(msg => {
+        const fetchedMsg = msg.first();
+        fetchedMsg.edit({
+          embeds: [
+            {
+              title: `Connected Bots`,
+              description: "successfully activated " + sentProjects + " proxies, " + failedProxies + " failed to load."
+            }
+          ]
+        });
+    });
+      } else {
+        message.reply("You cant use that.");
       }
 
       break;
