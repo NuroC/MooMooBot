0955627dd2d0b632dbad8cba41746e614da9f741
Nuro <82734446+NuroC@users.noreply.github.com>
2021-12-25 16:10:25 -0700

    Create removeBots.js

---
+++ src/strings/embeds/removeBots.js
@@ -0,0 +1,12 @@
+function succBots(args) {
+  return {
+    embeds: [
+      {
+        title: `Removing Bots`,
+        description: "Bots in the server will be removed as soon as possible."
+      }
+    ]
+  };
+}
+
+module.exports = succBots;
