75756499d0b024504ee5a49281321b940c67dc28
Nuro <82734446+NuroC@users.noreply.github.com>
2021-12-26 11:25:54 -0700

    Update generate.js

--- src/functions/generate.js
+++ src/functions/generate.js
@@ -1,16 +1,2 @@
-const getToken = async page => {
-    return page.evaluate(async () => {
-      return new Promise(resolve => {
-        window.grecaptcha
-          .execute("6LevKusUAAAAAAFknhlV8sPtXAk5Z5dGP5T2FYIZ", {
-            action: "homepage"
-          })
-          .then(function(e) {
-            console.log(e);
-            resolve(e);
-          });
-      });
-    });
-};
 
 module.exports = getToken;
