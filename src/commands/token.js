d25f95358be1360e3530263dbecb7053eef40b6f
Nuro <82734446+NuroC@users.noreply.github.com>
2021-12-26 10:51:14 -0700

    Update token.js

--- src/commands/token.js
+++ src/commands/token.js
@@ -1,15 +1,13 @@
-async function executeCommand(message, init, Generate, page, getTaken) {
-        if (!init) return message.channel.send("Puppeeter isn't in the browser.");
-      let StartDate = Date.now();
-      let Result = await Generate(page);
-      message.channel.send({
-        embeds: [
-          {
-            title: `Execution took ${getTaken(StartDate)} seconds`,
-            description: Result
-          }
-        ]
-      });
+async function executeCommand(e, n, t, o, s) {
+    if (!n) return e.channel.send("Puppeeter isn't in the browser.");
+    let c = Date.now(),
+        i = await t(o);
+    e.channel.send({
+        embeds: [{
+            title: `Execution took ${s(c)} seconds`,
+            description: i
+        }]
+    });
 }
 
-module.exports = executeCommand
+module.exports = executeCommand;
